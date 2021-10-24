68c4ef074df259d9248afbd1e54a47aa5f0d94b8
edvanbarros <edvanbarros@users.noreply.github.com>
2021-10-24 13:37:41 -0300

    Update webscrapyStatusIXbr.py
    Restruturado a procura dos elementos HTML. Refeito o loop for range para coletar todos os IX e o Status informando na pagina.

--- webscrapyStatusIXbr.py
+++ webscrapyStatusIXbr.py
@@ -4,13 +4,8 @@ import requests
 html = requests.get("https://status.ix.br").content
 soup = BeautifulSoup(html, "html.parser")
 
-servico = soup.find_all('div', class_='group-items hide')
+servico = soup.find_all("li", class_='list-group-item sub-component')
 
-name_ix0 = servico[0].li.text.strip()
-name_ix1 = servico[1].li.text.strip()
-name_ix2 = servico[2].li.text.strip()
-name_ix3 = servico[3].li.text.strip()
-print(name_ix0[:41], ":", servico[0].li.small.text)
-print(name_ix1[:43], ":", servico[1].li.small.text)
-print(name_ix2[:41], ":", servico[2].li.small.text)
-print(name_ix3[:17], ":", servico[3].li.small.text)
+for i in range(len(servico)):
+    name_ix = servico[i].text.strip()
+    print(servico[i].text.strip().strip("Operacional").strip("Matriz de Comutação - ").strip(), ":", servico[i].div.text.strip().replace(" ", ""))
